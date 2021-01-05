583c082082d34ff7a17a9dd0bac4a9418e8b7550
geoffreyadam <geoffrey.adam@ymail.com>
2021-01-05 22:59:25 +0100

    [UPDATE] Ending menu. TODO : Change colors + links
     Modifications qui seront validées :
    	modifié :         src/assets/styles/header.css
    	modifié :         src/components/header/Header.vue

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,22 +1,40 @@
 <template>
   <div class="header">
     <section>
-      <router-link to="/" v-on:mouseover.native="changeHoveredIcon('home')" >
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#B5BDD6" class="bi bi-house-door" viewBox="0 0 16 16">
+      <router-link to="/">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FD6C26" class="bi bi-house-door" viewBox="0 0 16 16">
           <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
         </svg>
       </router-link>
-      <section class="header_active"></section>
+      <div></div>
     </section>
     <section>
-      <router-link to="/about" v-on:mouseover.native="changeHoveredIcon('about')">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#B5BDD6" class="bi bi-person-circle" viewBox="0 0 16 16">
+      <router-link to="/">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FD6C26" class="bi bi-camera" viewBox="0 0 16 16">
+          <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"/>
+          <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+        </svg>
+      </router-link>
+      <div></div>
+    </section>
+        <section>
+      <router-link to="/">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FD6C26" class="bi bi-images" viewBox="0 0 16 16">
+          <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+          <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
+        </svg>
+      </router-link>
+      <div></div>
+    </section>
+    <section>
+      <router-link to="/about">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FD6C26" class="bi bi-person-circle" viewBox="0 0 16 16">
           <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
           <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
           <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
         </svg>
       </router-link>
-      <section class="header_active"></section>
+      <div></div>
     </section>
   </div>
 </template>
@@ -31,9 +49,9 @@ export default {
     }
   },
   methods: {
-    changeHoveredIcon: function (hoveredButton) {
-      this.hovered = this.hovered === hoveredButton ? this.hovered : hoveredButton
-    }
+  },
+  beforeMount () {
+    this.active = localStorage.header
   }
 }
 </script>
