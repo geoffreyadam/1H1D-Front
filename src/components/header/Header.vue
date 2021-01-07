1dee2b7040475b2074071e3c9cf8045987eb5cf9
geoffreyadam <geoffrey.adam@ymail.com>
2021-01-07 21:42:40 +0100

    [FEATURE] Adding axios for later Starting to create auth pages New font
     Modifications qui seront validées :
    	modifié :         package-lock.json
    	modifié :         package.json
    	modifié :         src/assets/styles/main.less
    	modifié :         src/components/header/Header.vue
    	modifié :         src/router/index.js
    	modifié :         src/views/Home.vue
    	nouveau fichier : src/views/auth/Register.vue

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -27,7 +27,7 @@
       <div></div>
     </section>
     <section>
-      <router-link to="/about">
+      <router-link to="/register">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FD6C26" class="bi bi-person-circle" viewBox="0 0 16 16">
           <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
           <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
---
+++ src/views/auth/Register.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="marged">
+    <h1>Créer un compte</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register'
+}
+</script>
