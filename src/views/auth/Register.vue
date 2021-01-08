c5365539dd292217fca3901f7c272c2f068a0714
geoffreyadam <geoffrey.adam@ymail.com>
2021-01-08 20:41:06 +0100

    [UPDATE] Adding new colors Register form created, still need to create some form validation, error display
     Modifications qui seront validées :
    	modifié :         src/assets/styles/header.less
    	modifié :         src/assets/styles/main.less
    	modifié :         src/views/auth/Register.vue

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,11 +1,66 @@
 <template>
-  <div class="marged">
+  <section class="marged">
     <h1>Créer un compte</h1>
-  </div>
+    <form action="">
+      <section>
+        <label for="email">Email</label>
+        <input type="email" id ="email">
+      </section>
+      <section>
+        <label for="password">
+          <span>Mot de passe</span>
+          <div v-on:click="displayPassword('password')">
+            <svg v-show="hidePassword === 'password'" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+              <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+              <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+            </svg>
+            <svg v-show="hidePassword === 'text'" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+              <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.027 7.027 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.088z"/>
+              <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6l-12-12 .708-.708 12 12-.708.707z"/>
+            </svg>
+          </div>
+        </label>
+        <input :type="hidePassword" id="password">
+      </section>
+      <section>
+        <label for="password_confirm">
+          <span>Confirmation mot de passe</span>
+          <div v-on:click="displayPassword('password_confirm')">
+            <svg v-show="hideConfirmPassword === 'password'" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+              <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+              <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+            </svg>
+            <svg v-show="hideConfirmPassword === 'text'" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+              <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.027 7.027 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.088z"/>
+              <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6l-12-12 .708-.708 12 12-.708.707z"/>
+            </svg>
+          </div>
+        </label>
+        <input :type="hideConfirmPassword" id="password_confirm">
+      </section>
+      <button>Créer un compte</button>
+    </form>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'Register'
+  name: 'Register',
+  data: function () {
+    return {
+      hidePassword: 'password',
+      hideConfirmPassword: 'password'
+    }
+  },
+  methods: {
+    displayPassword: function (id) {
+      console.log(id)
+      if (id === 'password') {
+        this.hidePassword = this.hidePassword === 'password' ? 'text' : 'password'
+      } else if (id === 'password_confirm') {
+        this.hideConfirmPassword = this.hideConfirmPassword === 'password' ? 'text' : 'password'
+      }
+    }
+  }
 }
 </script>
