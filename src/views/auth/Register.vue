d21ab1472cf3d01f9cb6507d11b6d5fe5ce4aec4
geoffreyadam <geoffrey.adam@ymail.com>
2021-01-12 22:42:23 +0100

    [UPDATE] Form validation added Now displaying errors on form validate
     Modifications qui seront validées :
    	modifié :         src/assets/styles/main.less
    	modifié :         src/views/auth/Register.vue

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -2,9 +2,9 @@
   <section class="marged">
     <h1>Créer un compte</h1>
     <form action="" v-on:submit.prevent="checkForm">
-      <input-type v-model="email" type="email" name="Email" id="email"></input-type>
-      <input-type v-model="password" type="password" name="Mot de passe" id="password" restriction="Minimum 8 caractères"></input-type>
-      <input-type v-model="confirmPassword" type="password" name="Confirmer le mot de passe" id="confirm_password" restriction="Minimum 8 caractères"></input-type>
+      <input-type v-model="email" type="email" name="Email" id="email" :error='emailError'></input-type>
+      <input-type v-model="password" type="password" name="Mot de passe" id="password" restriction="Minimum 8 caractères" :error='passwordError'></input-type>
+      <input-type v-model="confirmPassword" type="password" name="Confirmer le mot de passe" id="confirm_password" restriction="Minimum 8 caractères" :error='confirmPasswordError'></input-type>
       <button>Créer un compte</button>
     </form>
   </section>
@@ -22,21 +22,34 @@ export default {
     return {
       email: '',
       password: '',
-      confirmPassword: ''
+      confirmPassword: '',
+      emailError: '',
+      passwordError: '',
+      confirmPasswordError: ''
     }
   },
   methods: {
-    displayPassword: function (id) {
-      if (id === 'password') {
-        this.hidePassword = this.hidePassword === 'password' ? 'text' : 'password'
-      } else if (id === 'password_confirm') {
-        this.hideConfirmPassword = this.hideConfirmPassword === 'password' ? 'text' : 'password'
-      }
-    },
     checkForm: function () {
-      console.log(this.email)
-      console.log(this.password)
-      console.log(this.confirmPassword)
+      if (this.email.length === 0) {
+        this.emailError = 'Veuillez renseigner un email'
+      } else {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (!re.test(String(this.email).toLowerCase())) {
+          this.emailError = 'Email invalide'
+        } else {
+          this.emailError = ''
+        }
+      }
+      this.passwordError = this.password.length < 8 ? '8 caractères minimum' : ''
+      this.confirmPasswordError = this.confirmPassword.length < 8 ? '8 caractères minimum' : ''
+      this.confirmPasswordError = this.confirmPassword !== this.password ? 'Les mots de passe doivent être identiques' : ''
+      this.passwordError = this.password !== this.confirmPassword ? 'Les mots de passe doivent être identiques' : ''
+
+      if (this.emailError === '' && this.passwordError === '' && this.confirmPasswordError === '') {
+        console.log('Form OK')
+      } else {
+        console.log('Form not OK')
+      }
     },
     changeUsername (username) {
       this.user.username = username
